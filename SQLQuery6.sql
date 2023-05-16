675bf0e7445a516b4d6cdfdc6582b18a53397fca
sujith-sairam <89727004+sujith-sairam@users.noreply.github.com>
2023-05-16 08:55:59 +0530

    Add files via upload

--- SQLQuery6.sql
+++ SQLQuery6.sql
@@ -91,9 +91,10 @@ insert into tbl_building values(7,'sujith','coimbatore',2,'6381212770','saisujit
 insert into tbl_building values(2,'vignesh','coimbatore',2,'9876543210','vignesh@gmail.com')
 insert into tbl_building values(3,'sathashivam','coimbatore',1,'6798543210','sathashivam@gmail.com')
 insert into tbl_building values(4,'Nicholas','Yercaud',1,'6798543210','nick@gmail.com')
-insert into tbl_building values(5,'Micholas','Yercaud',1,'6798543210','nick@gmail.com')
+insert into tbl_building values(5,'Micholas','',1,'6798543210','nick@gmail.com')
+insert into tbl_building values(10,'Nicholas Pooran','',1,'6798543210','nick@gmail.com')
 
-select * from tbl_building
+
 
 select * from tbl_building order by owner_name asc
 
@@ -137,7 +138,11 @@ insert into tbl_bill values(6,1,'01','2022','2022-04-05',10050,110000,1,'2017-12
 
 insert into tbl_bill values(9,1,'12','2017','2022-04-05',1050,110000,0,'2017-12-01',400)
 
-insert into tbl_bill values(8,1,'12','2017','2022-04-05',150,110000,1,'2017-12-01',400)
+insert into tbl_bill values(11,1,'11','2017','2022-04-05',1250,110,1,'2017-12-01',90)
+
+insert into tbl_bill values(12,2,'10','2017','2022-04-05',1250,110,1,'2017-12-01',120)
+
+insert into tbl_bill values(13,1,'11','2019','2022-04-05',150,110,1,'2017-09-23',120)
 
 delete from tbl_bill
 
@@ -212,3 +217,182 @@ join tbl_bill on tbl_bill.meter_id = tbl_meter.id
 group by meter_number,owner_name,building_address
 having count(is_payed) > 0
 order by owner_name
+
+
+  -------------------------------------------------------- AGGREGATE FUNCTIONS ---------------------------------------------------- 
+   create database purchase
+
+   use  purchase
+	  CREATE TABLE customers (
+	  id INT PRIMARY KEY,
+	  customer_name VARCHAR(50),
+	  address VARCHAR(100),
+	  phone_number VARCHAR(15));
+
+	CREATE TABLE orders (
+	  order_id INT PRIMARY KEY,
+	  order_date DATE,
+	  customer_id INT,
+	  salesman_name VARCHAR(100),
+	  purchase_amount DECIMAL(10, 2),
+	  FOREIGN KEY (customer_id) REFERENCES customers(id));
+	
+	
+	-- Insert values into the customers table
+	INSERT INTO customers (id, customer_name, address, phone_number)
+	VALUES (1, 'John Doe', '123 Main St', '555-1234'),
+		   (2, 'Jane Smith', '456 Elm St', '555-5678'),
+		   (3, 'Robert Johnson', '789 Oak St', '555-9101');
+
+	-- Insert values into the orders table
+	INSERT INTO orders (order_id, order_date, customer_id, salesman_name, purchase_amount)
+	VALUES (1, '2023-05-01', 1, 'Tom', 100.00),
+		   (2, '2023-05-02', 1, 'Tom', 75.50),
+		   (3, '2023-05-03', 2, 'Sarah', 200.00);
+
+	
+  select sum(purchase_amount) as total_purchase_amount from orders
+
+  select avg(purchase_amount) as total_purchase_amount from orders
+
+  select min(purchase_amount) as total_purchase_amount from orders
+
+  select max(purchase_amount) as total_purchase_amount from orders
+
+  select COUNT(salesman_name) from orders
+
+  select id,max(purchase_amount) from customers 
+  join orders on orders.customer_id = customers.id
+  group by id
+ 
+  
+  --------------------------------------------------  WILDCARDS EXERCISES --------------------------------------------------------
+
+  select * from salesman
+  where city like 'Paris' or city like 'Rome'
+
+  select salesman_id,salesman_name,city from salesman
+  where city not like 'Paris' and city not like 'Rome'
+
+  select * from customers
+  where customer_id >= 3007 and customer_id <= 3009
+
+  select * from salesman
+  where commision >= 0.12 and commision <= 0.14
+
+  select * from orders
+  where purchase_amount between 500 and 4000 and purchase_amount not in (948.50,1983.43)
+
+  select * from saleseman
+  where salesman_name like 'A%K'
+
+  select * from saleseman
+  where salesman_name not like 'A%L'
+
+  select * from customer
+  where customer_name like 'B%'
+
+  select * from customer
+  where customer_name like '%n'
+
+
+  -------------------------------------------------------  STRING EXERCISES -----------------------------------------------------------
+
+
+  select owner_name ,len(owner_name) as name_length from tbl_building
+  order by owner_name
+
+  select owner_name,contact_number from tbl_building
+  where len(owner_name) = (select min(len(owner_name))  from tbl_building) order by owner_name
+	
+  select owner_name,concat(contact_number,' - ',email_address) as contact_details from tbl_building
+
+  select left(meter_number,3) as meter_code from tbl_meter
+  order by meter_code desc
+
+  select left(owner_name,3) as name_code , contact_number from tbl_building
+  order by owner_name
+
+  select * from tbl_building_type
+  where lower(right(building_type_name,1)) = 'l' order by building_type_name
+
+  select * from tbl_building
+  where owner_name like '%di%' order by owner_name
+
+
+
+  -------------------------------------------------- BOOLEAN AND RELATIONAL OPERATORS ------------------------------------------------------- 
+
+
+  select order_name,order_date,purchase_amount from salesman
+  where salesman_id = 5001 
+
+  select * from customers
+  where grade > 100
+
+  select * from customers
+  where city = 'New York' and grade > 100 
+
+  select * from customers
+  where not city = 'New York' or grade > 100
+
+  select * from customers
+  where city = 'New York' or not grade > 100
+
+  select salesman_id , salesman_name , city from salesman 
+  where commission > 0.10 and commission < 0.12
+
+  select * from orders
+  where (purch_amt<200 or not (ord_date>='2012-02-10' and customer_id<3009))
+
+  select * from employees
+  where department = 47 or department = 63
+
+	------------------------------------------------------- SET AND UNION -----------------------------------------------------------------------
+
+	select * from salesman
+	where city='London'union
+	select *from customer
+	where city='London'
+
+
+	select distinct salesman_name,cities from salesman
+	union
+	select distinct salesman_name,cities from customer
+
+
+	select * from Salesman
+	union
+	select * from Customer;
+
+
+	select order_date, salesman_id, MAX(order_amount) as largest_order from orders 
+	group by order_date, salesman_id
+	union
+	select order_date, salesman_id, MIN(order_amount) as smallest_order from orders 
+	group by order_date, salesman_id
+
+
+	select s.salesman_name, s.city, 'Has Customers' as status from Salesman s
+	INNER JOIN Customer c on s.city = c.city
+	union
+	select s.salesman_name, s.city, 'No Customers' as status
+	from Salesman s
+	where s.city NOT IN (select distinct city from Customer)
+
+
+
+
+	----------------------------------------------------- DATE AND TIME ------------------------------------------------------------------------
+
+  select * from tbl_bill
+
+  select count(id) as bill_payed from tbl_bill
+  where is_payed = 1 and _month = 10 and _year = 2017
+
+  select fine_amount as total_fine_amount from tbl_bill
+  where _month between 10 and 11
+
+  select * from tbl_bill
+  where payment_date = '2017-09-23'
+
